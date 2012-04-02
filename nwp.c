3146a83847b04e9bfce8252ea0482f6f27cc1b99
unknown <alior@alior-PC.(none)>
2012-04-02 15:02:08 +0200

    cyk

--- nwp.c
+++ nwp.c
@@ -3,21 +3,42 @@
 #include <string.h>
 
 void LCSLENGTH(char X[], char Y[]){
-
+	int m=strlen(X), n=strlen(Y), i, j, c[m][n], b[m][n];
+	for (i=1;i<m;i++)
+		c[i][0]=0;
+	for (j=0;j<n;j++)
+		c[0][j]=0;
+	for (i=1;i<m;i++){
+		for(j=1;j<n;j++){
+			if(X[i]==Y[j]){
+				c[i][j]=c[i-1][j-1]+1;
+				b[i][j]=0;
+			}
+			else
+				if(c[i-1][j]>=c[i][j-1]){
+					c[i][j]=c[i-1][j];
+					b[i][j]=1;
+				}
+				else{
+					c[i][j]=c[i][j-1];
+					b[i][j]=2;
+				}
+		}
+	}
+	printf("dlugosc nwp: %d ", c[m][n]);
 }
+
 int main(){
 
-	char X[20], Y[20];
+	char X[10], Y[10];
 	int i=0;
 	for(i=0; i<20;i++){
-		X[i]=0;
-		Y[i]=0;
+		X[i]=-1;
+		Y[i]=-1;
 	}
 	printf("Dawaj ciagi \n");
 	scanf("%s", X);
 	scanf("%s", Y);
-	printf("%d",strlen(X));
-	printf("%d",strlen(Y));
 //	for(i=0;X[i]!=0;i++){
 //		printf("%c",X[i]);
 //	}
