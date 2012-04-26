49de218d4424d2545c26afd9c179bc29f425cf6c
unknown <alior@alior-PC.(none)>
2012-04-26 02:45:53 +0200

    main

--- list.c
+++ list.c
@@ -1,15 +1,15 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+#define NIL NULL
+
 typedef struct W {
 		int key, length;
 		struct W *head, *next, *last;
 		int kolor;
 	} wezel;
 
-int main(){
-	return 0;
-}
+wezel NIL;
 
 wezel* MakeSet(int k){
 	wezel *x;
@@ -17,7 +17,7 @@ wezel* MakeSet(int k){
 	x->key=k;
 	x->head=x;
 	x->last=x;
-	x->next=x;
+	x->next=NIL;
 	x->length=1;
 	return x;
 }
@@ -27,7 +27,42 @@ wezel* FindSet(wezel *x){
 }
 
 void Union(wezel *x, wezel *y){
+	wezel *pom;
 	if(y->length > x->length){
-
+		pom=x;
+		x=y;
+		y=x;
+	}
+	x->length = x->length + y->length;
+	x->last->next=y;
+	x->last=y->last;
+	while(y!=NIL){
+		y->head=x;
+		printf("reprezentantem jest %d \n", y->head->key);
+		y=y->next;
 	}
 }
+
+int main(){
+	wezel *W[10];
+	int i;
+	for(i=0;i<10;i++){
+		W[i]=MakeSet(i);
+	}
+	Union(FindSet(W[0]),FindSet(W[2]));
+	Union(FindSet(W[1]),FindSet(W[2]));
+	Union(FindSet(W[3]),FindSet(W[4]));
+	Union(FindSet(W[5]),FindSet(W[6]));
+	Union(FindSet(W[7]),FindSet(W[8]));
+	Union(FindSet(W[9]),FindSet(W[10]));
+	Union(FindSet(W[1]),FindSet(W[4]));
+	Union(FindSet(W[2]),FindSet(W[5]));
+	Union(FindSet(W[8]),FindSet(W[1]));
+	for(i=0;i<10;i++){
+			printf("%d \n", W[i]);
+		}
+	getchar();
+	getchar();
+	return 0;
+}
+
