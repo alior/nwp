14fe9fa64b01bd7dc494f20b88edcd19cdb14038
unknown <alior@alior-PC.(none)>
2012-06-05 23:30:22 +0200

    mnostwo poprawek

--- huff.c
+++ huff.c
@@ -4,20 +4,57 @@
 #include <math.h>
 
 typedef struct W {
-	int key;
-	struct W *left, *right, *p;
+	int key,symbol;
+	struct W *left, *right;
+	//int left, right;
 } wezel;
 
-void huffman(int A[]){
+void show(wezel *z, int pom){
+	int i;
+	printf("sym: %d wart: %d -> ", z->symbol,z->key);
+	for(i=0;i<2*pom-1;i++){
+		printf("sym: %d wart: %d -> ", z->left->symbol,z->left->key);
+	}
 
+}
+wezel *findmin(wezel *TAB[], int pom){
+	int i,x=2147483647;
+	wezel *szukany;
+	for(i=0;i<pom*2-1;i++){
+		//printf("\nSUPER 2\n;");
+		if(TAB[i]->key < x && TAB[i]->key > 0)
+			x=i;
+	}
+	szukany=TAB[x];
+	return szukany;
+}
 
+void huffman(wezel *TAB[], int pom){
+	int i;
+	wezel *z, *x, *y;
+	for(i=1;i<pom-1;i++){
+		//printf("\nSUPER 1\n;");
+		z=malloc(sizeof(wezel));
+		x=findmin(TAB,pom);
+		//printf("\nSUPER 3\n;");
+		z->left=x;
+		y=findmin(TAB,pom);
+		z->right=y;
+		z->key=x->key+y->key;
+		z->symbol=-2;
+		TAB[i+pom]=z;
+	}
+	for(i=0;i<pom*2-1;i++){
+			printf("sym:%c, key:%d || ",TAB[i]->symbol, TAB[i]->key);
+		}
+	//show(z,pom);
 }
 
 int main(int argc, char **argv){
-	int A[256],i;
+	int A[256],i,dlugosc,pom=0,j=0;
 	char x;
+	wezel *kurwa;
 	FILE *fp;
-	int dlugosc;
 	for(i=0;i<256;i++){
 		A[i]=0;
 	}
@@ -32,11 +69,36 @@ int main(int argc, char **argv){
 	fseek(fp,0,0);
 	for(i=0;i<dlugosc;i++){
 		fscanf(fp, "%c", &x);
-		printf("%d ",x);
 		A[x]++;
 	}
-	printf("\nliczba A: %d, B: %d, D: %d", A['A'],A['B'],A['D']);
-
+	for(i=0;i<256;i++){
+		if(A[i]!=0)
+			pom++;
+	}
+	wezel *TAB[pom*2-1];
+	kurwa=malloc(sizeof(wezel));
+	kurwa->symbol=0;
+	kurwa->key=0;
+	kurwa->left=NULL;
+	kurwa->right=NULL;
+	for(i=0;i<pom*2;i++){
+		TAB[i]=kurwa;
+	}
+	for(i=0;i<256;i++){
+		if(A[i]!=0){
+			wezel *z=malloc(sizeof(wezel));
+			z->key=A[i];
+			z->symbol=i;
+			z->left=NULL;
+			z->right=NULL;
+			TAB[j]=z;
+			j++;
+		}
+	}
+	/*for(i=0;i<pom*2-1;i++){
+		printf("sym:%c, key:%d || ",TAB[i]->symbol, TAB[i]->key);
+	}*/
+	huffman(TAB,pom);
 	fclose (fp);
 	return 0;
 }
